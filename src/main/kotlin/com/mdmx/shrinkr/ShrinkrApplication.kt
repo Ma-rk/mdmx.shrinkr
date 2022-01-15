6e172ead3e5e6e33e891c85d653872262cb0b1a0
Ma-rk <kim.mark.dev@gmail.com>
2022-01-15 13:32:46 +0900

    MDMX SHRINKER INITIALIZED

---
+++ src/main/kotlin/com/mdmx/shrinkr/ShrinkrApplication.kt
@@ -0,0 +1,11 @@
+package com.mdmx.shrinkr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShrinkrApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShrinkrApplication>(*args)
+}
---
+++ src/test/kotlin/com/mdmx/shrinkr/ShrinkrApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mdmx.shrinkr
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShrinkrApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
