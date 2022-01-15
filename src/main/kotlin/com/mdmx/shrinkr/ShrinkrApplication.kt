9607f886fe0541e2cebebc724cd3f02af2f29ea6
Ma-rk <kim.mark.dev@gamil.com>
2022-01-15 14:20:15 +0900

    코틀린 인덴트 변경(4->2)

--- src/main/kotlin/com/mdmx/shrinkr/ShrinkrApplication.kt
+++ src/main/kotlin/com/mdmx/shrinkr/ShrinkrApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class ShrinkrApplication
 
 fun main(args: Array<String>) {
-	runApplication<ShrinkrApplication>(*args)
+  runApplication<ShrinkrApplication>(*args)
 }
