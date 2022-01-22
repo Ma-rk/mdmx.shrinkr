56029693a5b49a8f0252bfe1d4ce81cf4c1ecc8d
Ma-rk <kim.mark.dev@gamil.com>
2022-01-23 06:36:05 +0900

    단축주소를 DB에 입력

--- src/main/kotlin/com/mdmx/shrinkr/service/ShrinkUrlService.kt
+++ src/main/kotlin/com/mdmx/shrinkr/service/ShrinkUrlService.kt
@@ -3,8 +3,10 @@ package com.mdmx.shrinkr.service
 import com.mdmx.shrinkr.ety.req.ShrinkUrlReqEty
 import com.mdmx.shrinkr.model.ShortUrlModel
 import com.mdmx.shrinkr.repository.ShortUrlRepository
+import com.mdmx.shrinkr.util.addPaddingCharacter
 import mu.KLogger
 import mu.KotlinLogging
+import org.hashids.Hashids
 import org.springframework.stereotype.Component
 
 @Component
@@ -12,9 +14,20 @@ class ShrinkUrlService(
   private val shortUrlRepository: ShortUrlRepository,
   private val lg: KLogger = KotlinLogging.logger {}
 ) {
+  private val hashids: Hashids = Hashids("my heart draws a dream")
+
   fun shrinkUrlViaHashids(shrinkUrlReqEty: ShrinkUrlReqEty): String {
 
     val shortUrlModel = ShortUrlModel(shrinkUrlReqEty.urlToShrink)
+    shortUrlRepository.save(shortUrlModel)
+
+    val seq = shortUrlModel.urlSeq
+    val hashedUrl = seq?.let { hashids.encode(it) }
+    val paddedUrl = hashedUrl?.let { addPaddingCharacter(it, 7) }
+
+    shortUrlModel.shortUrl = hashedUrl
+    shortUrlModel.paddedShortUrl = paddedUrl
+
     shortUrlRepository.save(shortUrlModel)
 
     val result = "saved seq: ${shortUrlModel.urlSeq}"
