cea4257bc8fbd2bcae261f683d52eebf5b948363
Ma-rk <kim.mark.dev@gamil.com>
2022-01-23 07:28:48 +0900

    addPaddingCharacter() 추가

--- src/main/kotlin/com/mdmx/shrinkr/util/UtilFun.kt
+++ src/main/kotlin/com/mdmx/shrinkr/util/UtilFun.kt
@@ -16,3 +16,20 @@ fun getRandomString(targetStringLength: Long): String {
     .collect({ StringBuilder() }, java.lang.StringBuilder::appendCodePoint, java.lang.StringBuilder::append)
     .toString()
 }
+
+/**
+ * https://www.geeksforgeeks.org/generate-random-string-of-given-size-in-java/
+ */
+fun addPaddingCharacter(originalStr: String, designatedLength: Int): String {
+  val alphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvxyz0123456789"
+  if (designatedLength <= originalStr.length) {
+    return originalStr
+  }
+  val loopIndicator = designatedLength - 1
+  val sb = StringBuilder(originalStr.length)
+  for (i in originalStr.length..loopIndicator) {
+    val index = (alphaNumericString.length * Math.random()).toInt()
+    sb.append(alphaNumericString[index])
+  }
+  return originalStr + sb.toString()
+}
