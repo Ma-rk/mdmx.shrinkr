464ed2715b999b2dba142ddcd2df7370d5806d17
Ma-rk <kim.mark.dev@gamil.com>
2022-01-22 22:37:44 +0900

    swagger에서 사용하는 파라미터 기본값 추가

--- src/main/kotlin/com/mdmx/shrinkr/ety/req/ShrinkUrlReqEty.kt
+++ src/main/kotlin/com/mdmx/shrinkr/ety/req/ShrinkUrlReqEty.kt
@@ -6,12 +6,12 @@ import io.swagger.annotations.ApiModelProperty
  * url 단축 리퀘스트 바디를 담기 위한 entity
  */
 class ShrinkUrlReqEty(
-  @ApiModelProperty(required = true, notes = "단축 대상 url")
+  @ApiModelProperty(required = true, notes = "단축 대상 url", example = "https://github.com/Ma-rk/")
   val urlToShrink: String,
 
-  @ApiModelProperty(required = false, notes = "단축 url 을 삭제할 수 있는 링크 요청 여부")
+  @ApiModelProperty(required = false, notes = "단축 url 을 삭제할 수 있는 링크 요청 여부", example = true.toString())
   val deletionLinkChecked: Boolean,
 
-  @ApiModelProperty(required = false, notes = "단축 url 삭제를 위한 링크를 받을 email 주소")
+  @ApiModelProperty(required = false, notes = "단축 url 삭제를 위한 링크를 받을 email 주소", example = "kim.mark.dev@gmail.com")
   val emailAddr: String
 )
