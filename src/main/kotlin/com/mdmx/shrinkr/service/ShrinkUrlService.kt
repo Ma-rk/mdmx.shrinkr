81507b9ffaaefd8c79788c16d664f3cfb28dff17
Ma-rk <kim.mark.dev@gamil.com>
2022-01-23 16:05:26 +0900

    처리 결과를 resp 객체에 담아 리턴

--- src/main/kotlin/com/mdmx/shrinkr/service/ShrinkUrlService.kt
+++ src/main/kotlin/com/mdmx/shrinkr/service/ShrinkUrlService.kt
@@ -1,6 +1,7 @@
 package com.mdmx.shrinkr.service
 
 import com.mdmx.shrinkr.ety.req.ShrinkUrlReqEty
+import com.mdmx.shrinkr.ety.resp.ShrinkUrlRespEty
 import com.mdmx.shrinkr.model.ShortUrlModel
 import com.mdmx.shrinkr.repository.ShortUrlRepository
 import com.mdmx.shrinkr.util.addPaddingCharacter
@@ -8,6 +9,7 @@ import com.mdmx.shrinkr.util.getRandomString
 import mu.KLogger
 import mu.KotlinLogging
 import org.hashids.Hashids
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 
 @Component
@@ -17,7 +19,8 @@ class ShrinkUrlService(
 ) {
   private val hashids: Hashids = Hashids("my heart draws a dream")
 
-  fun shrinkUrlViaHashids(shrinkUrlReqEty: ShrinkUrlReqEty): String {
+  fun shrinkUrlViaHashids(shrinkUrlReqEty: ShrinkUrlReqEty): ShrinkUrlRespEty {
+    lg.info { "Got $shrinkUrlReqEty" }
 
     val shortUrlModel = ShortUrlModel(shrinkUrlReqEty.urlToShrink)
     shortUrlRepository.save(shortUrlModel)
@@ -36,8 +39,20 @@ class ShrinkUrlService(
 
     shortUrlRepository.save(shortUrlModel)
 
+    val muMap = mutableMapOf<String, String?>()
+    muMap["urlToShrink"] = shrinkUrlReqEty.urlToShrink
+    if (shrinkUrlReqEty.deletionLinkChecked) muMap["creatorEmailAddr"] = shrinkUrlReqEty.emailAddr
+    muMap["shortUrl"] = "https://mdmx.xyz/re/$paddedUrl"
+
+    val shrinkUrlRespEty = ShrinkUrlRespEty(
+      HttpStatus.OK,
+      HttpStatus.OK.value(),
+      "SUCCESS. URL has been successfully shrunken",
+      muMap
+    )
+    lg.info("Result of Shrinking: $shrinkUrlRespEty")
     val result = "saved seq: ${shortUrlModel.urlSeq}"
     lg.info { result }
-    return result;
+    return shrinkUrlRespEty
   }
 }
