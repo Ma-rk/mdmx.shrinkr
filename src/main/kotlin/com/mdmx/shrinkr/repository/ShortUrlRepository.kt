508e021b5db923be6dc71db9ee272424e8682463
Ma-rk <kim.mark.dev@gamil.com>
2022-01-23 16:57:00 +0900

    findOneByPaddedShortUrl() 추가

--- src/main/kotlin/com/mdmx/shrinkr/repository/ShortUrlRepository.kt
+++ src/main/kotlin/com/mdmx/shrinkr/repository/ShortUrlRepository.kt
@@ -4,4 +4,5 @@ import com.mdmx.shrinkr.model.ShortUrlModel
 import org.springframework.data.repository.CrudRepository
 
 interface ShortUrlRepository : CrudRepository<ShortUrlModel, Long> {
+  fun findOneByPaddedShortUrl(padded_short_url: String): ShortUrlModel
 }
